9738645336253e899a4aef80170b98682d615fde
gabsouki <soucy.gabriel@gmail.com>
2017-11-21 14:25:54 -0500

    Initial Commit

---
+++ Koodo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Koodo
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
