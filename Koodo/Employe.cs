1ab31fef9474a397cce32e2012376941860130a6
Jeanne <Jeanne@MAGIFIQUE>
2017-11-21 17:51:08 -0500

    employe

--- Koodo/Employe.cs
+++ Koodo/Employe.cs
@@ -6,7 +6,22 @@ using System.Threading.Tasks;
 
 namespace Koodo
 {
-    class Employe
+    abstract class Employe
     {
+        private string nom;
+        private string prenom;
+        private string cell;
+        private int salaire;
+        private List<Employe> entres;
+        public abstract int Salaire();
+        Employe(string nom, string prenom, string cell, int salaire)
+        {
+            this.nom = nom;
+            this.prenom = prenom;
+            this.cell = cell;
+            this.salaire = salaire;
+        }
+
+
     }
 }
